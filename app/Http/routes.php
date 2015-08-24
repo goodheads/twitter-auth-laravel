20e7ad7922a17a3c0ddf85bdad7234e2822f9198
busayo <prosperotemuyiwa@gmail.com>
2015-08-24 17:50:00 +0100

    Add twitter route handler to routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('auth/twitter', 'Auth\AuthController@redirectToProvider');
+Route::get('auth/twitter/callback', 'Auth\AuthController@handleProviderCallback');
