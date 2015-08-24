cf6bacf86dcef919e027d7b12cc2ea81c06fc630
busayo <prosperotemuyiwa@gmail.com>
2015-08-24 22:56:07 +0100

    Change email to twitter handle

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,7 +43,7 @@
                     You are Logged In
                 <div>
                    <h4>Your name is  {{ Auth::user()->name }} </h4>
-                   <h4>Your email is {{ Auth::user()->email }} </h4>
+                   <h4>Your twitter handle is {{ Auth::user()->handle }} </h4>
                    <img src="{{ Auth::user()->avatar }}" height="200" width="200" />
                 </div>
             </div>
